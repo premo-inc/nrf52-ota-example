acf5fd612b21094d4d2fe2972be812ef7599ca08
21km43 <mizumoto@premo-inc.com>
2025-06-13 11:39:12 +0900

    add verify

--- package/program.bat
+++ package/program.bat
@@ -1,5 +1,5 @@
 @echo off
 
 nrfjprog --family NRF52 --eraseall
-nrfjprog --family NRF52 --program Output/all.hex
+nrfjprog --family NRF52 --program Output/all.hex --verify
 nrfjprog --family NRF52 --reset
